0fa3167b42bccd866bbff2e0b611b3665603a735
umberto-github <bachetti.u@gmail.com>
2025-03-29 21:11:39 +0100

    Corretto a Info Movie

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,7 +6,7 @@ function NavBar() {
         <>
             <nav className="navbar">
                 <div className="navbar-brand">
-                    <Link to="/">Movie Store , prova microfono</Link>
+                    <Link to="/">Info Movie</Link>
                 </div>
                 <div className="navbar-links">
                     <Link to="/" className="nav-link">Home</Link>
